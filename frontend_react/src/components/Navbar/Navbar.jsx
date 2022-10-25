e72e66d8334bc71c73c43aa50f7dc0a11157ff76
Hewad Noori <hewadnoori16@gmail.com>
2022-10-24 20:08:51 -0400

    added more files, scss for nav not done

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import {images} from '../../constants'
 import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
+          <li className='app__navbar-links' key={`link-${item}`}>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
