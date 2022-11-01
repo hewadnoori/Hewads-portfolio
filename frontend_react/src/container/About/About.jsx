e32f19953854b7ffc2f6a68d786dc73bed439e82
Hewad Noori <hewadnoori16@gmail.com>
2022-11-01 17:01:35 -0400

    fixed issue with scss not applying

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-
+import {images} from '../../constants'
 import { AppWrap, MotionWrap } from '../../Wrapper';
 import './About.scss';
-//import { urlFor, client } from '../../client';
+
+
+const abouts = [
+  {title: 'Front-end Web Developer', description: 'I am a good front-end developer.', imgUrl: images.about01},
+  {title: 'Web Design', description: 'I am a good web designer.', imgUrl: images.about02},
+  {title: 'Web Animations', description: 'I am a good at web animations.', imgUrl: images.about03},
+
+]
 
 const About = () => {
-//   const [abouts, setAbouts] = useState([]);
-
-  // useEffect(() => {
-  //   const query = '*[_type == "abouts"]';
-
-  //   client.fetch(query).then((data) => {
-  //     setAbouts(data);
-  //   });
-  // }, []);
 
   return (
     <>
@@ -29,7 +27,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
+            <img src={about.imgUrl} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
