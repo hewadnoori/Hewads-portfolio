40c6621ffd04ade22797fa550d0479f60513e895
Hewad Noori <hewadnoori16@gmail.com>
2022-11-03 18:53:08 -0400

    finished work section, will populate

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsLinkedin, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <BsLinkedin />
     </div>
     <div>
       <FaFacebookF />
